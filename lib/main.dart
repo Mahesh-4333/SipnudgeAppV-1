1063026b43c86c56df93252ff06f17b4fa4968e8
Newton Singh <newtonsingh@Newtons-MacBook-Pro.local>
2025-08-07 00:23:50 +0530

    updated commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart' show ScreenUtilInit;
+import 'package:flutterapp1/account&security.dart';
+import 'package:flutterapp1/achievement.dart';
+import 'package:flutterapp1/companymotto.dart';
+import 'package:flutterapp1/contactsupport.dart';
+import 'package:flutterapp1/dailygoalpage.dart';
+import 'package:flutterapp1/forgotpassword.dart';
+import 'package:flutterapp1/forgotpwdotppage.dart';
+import 'package:flutterapp1/help&support.dart';
+import 'package:flutterapp1/homeanalysis.dart';
 import 'package:flutterapp1/homepage.dart';
+import 'package:flutterapp1/lifestyleinfoscreen.dart';
+import 'package:flutterapp1/linkedaccounts.dart';
+import 'package:flutterapp1/loadingscreen.dart';
+import 'package:flutterapp1/otpscreen.dart';
+import 'package:flutterapp1/passwordupdatepage.dart';
+import 'package:flutterapp1/personalinfo.dart';
+import 'package:flutterapp1/personalinfoinprofile.dart'
+    show PersonalInfoInProfile;
+import 'package:flutterapp1/preferences.dart';
+import 'package:flutterapp1/profilescreen.dart';
+import 'package:flutterapp1/setnewpasswordscreen.dart';
+import 'package:flutterapp1/signin.dart';
+import 'package:flutterapp1/signin_signup.dart';
+import 'package:flutterapp1/signupblankpage.dart';
 //import 'package:flutterapp1/homepage.dart';
 
 void main() {
@@ -27,31 +50,31 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           initialRoute: '/',
           routes: {
-            //'/': (context) => const BadgeWithConcentricBackground(),
-            '/': (context) => HomePageScreen(),
-            // '/companymotto': (context) => CompanyMottoPage(),
-            // '/signin_signupsage': (context) => SignIn_SignUpPage(),
-            // '/signupblankpage': (context) => SignUpBlankPage(),
-            // '/signin': (context) => SignInPage(),
-            // '/forgotpassword': (context) => ForgotPassword(),
-            // '/personalinfo': (context) => PersonalInfoPage(),
-            // '/otpscreen': (context) => OtpScreen(),
-            // '/loadingscreen': (context) => LoadingPage(),
-            // '/dailygoalpage': (context) => DailyGoalPage(),
-            // '/lifestyleinfo': (context) => LifeStyleInfoPage(),
-            // '/forgotpasswordotp': (context) => ForgotPwdOtpPage(),
-            // '/newpasswordscreen': (context) => NewPasswordScreen(),
-            // '/newpasswordupdate': (context) => PasswordUpdatedScreen(),
-            // '/homepage': (context) => HomePageScreen(),
-            // '/profilescreen': (context) => ProfileScreenPage(),
-            // '/personalinfoinprofile': (context) => PersonalInfoInProfile(),
-            // '/preferences': (context) => PreferencesPage(),
-            // '/linked_accounts': (context) => LinkAccountsPage(),
-            // '/support': (context) => HelpAndSupportPage(),
-            // '/contact_support': (context) => CustomerSupportPage(),
-            // '/account_security': (context) => AccountAndSecurityPage(),
-            //'/analysis': (context) => HomeAnalysisPage(),
-            // '/achievement': (context) => BadgeWithConcentricBackground(),
+            '/': (context) => const BadgeWithConcentricBackground(),
+            '/': (context) => HomeAnalysisPage(),
+            '/companymotto': (context) => CompanyMottoPage(),
+            '/signin_signupsage': (context) => SignIn_SignUpPage(),
+            '/signupblankpage': (context) => SignUpBlankPage(),
+            '/signin': (context) => SignInPage(),
+            '/forgotpassword': (context) => ForgotPassword(),
+            '/personalinfo': (context) => PersonalInfoPage(),
+            '/otpscreen': (context) => OtpScreen(),
+            '/loadingscreen': (context) => LoadingPage(),
+            '/dailygoalpage': (context) => DailyGoalPage(),
+            '/lifestyleinfo': (context) => LifeStyleInfoPage(),
+            '/forgotpasswordotp': (context) => ForgotPwdOtpPage(),
+            '/newpasswordscreen': (context) => NewPasswordScreen(),
+            '/newpasswordupdate': (context) => PasswordUpdatedScreen(),
+            '/homepage': (context) => HomePageScreen(),
+            '/profilescreen': (context) => ProfileScreenPage(),
+            '/personalinfoinprofile': (context) => PersonalInfoInProfile(),
+            '/preferences': (context) => PreferencesPage(),
+            '/linked_accounts': (context) => LinkAccountsPage(),
+            '/support': (context) => HelpAndSupportPage(),
+            '/contact_support': (context) => CustomerSupportPage(),
+            '/account_security': (context) => AccountAndSecurityPage(),
+            '/analysis': (context) => HomeAnalysisPage(),
+            '/achievement': (context) => BadgeWithConcentricBackground(),
           },
         );
       },
